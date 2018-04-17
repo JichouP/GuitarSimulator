26a55ade08e94664f095e677c5ed85c4fe3d9a03
JichouP <akkunn100@gmail.com>
2018-04-17 20:59:23 +0900

    init

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+entry: path.resolve('src') + '/index.jsx',
+       output: {
+path: path.resolve('public'),
+      filename: 'bundle.js'
+       },
+module: {
+rules: [{
+test: /\.(js|jsx)$/,
+       loader: 'babel-loader',
+       options: {presets: ["env", "react"]}
+       }]
+        },
+resolve: {
+extensions: ['.jsx', '.js'],
+         },
+         // optimization: {
+         //   minimize: true,
+         // },
+devtool: 'source-map',
+         target: 'web'
+};
+
