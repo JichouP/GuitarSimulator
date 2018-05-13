5db8aca130b6f66de2b350fb25521e7e9147c103
JichouP <akkunn100@gmail.com>
2018-05-13 23:07:48 +0900

    画像を表示したい

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Sprite, Stage } from 'react-pixi-fiber';
-import App from './app';
+import * as PIXI from 'pixi.js';
+// import App from './app';
+const kussy = './res/kussy.jpg';
 
-render(<App />, document.getElementById('root'));
+const Kussy = props => (
+  <Sprite texture={PIXI.Texture.fromImage(kussy)} {...props} />
+);
+
+render(
+  <Stage width={800} height={600} options={{ backgroundColor: 0x10bb99 }}>
+    <Kussy x={100} y={100} />
+  </Stage>,
+  document.getElementById('root')
+);
+// render(<App />, document.getElementById('root'));
