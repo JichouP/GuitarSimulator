a0505c3c0d70c6fc934e6e7276ecb6e75291c2e7
JichouP <akkunn100@gmail.com>
2018-05-17 08:57:43 +0900

    やらかしが直った

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import { Sprite, Stage } from 'react-pixi-fiber';
 import * as PIXI from 'pixi.js';
 // import App from './app';
 const kussy = './res/kussy.jpg';
+// import kussy from '../dist/res/kussy.jpg';
 
-const Kussy = props => (
-  <Sprite texture={PIXI.Texture.fromImage(kussy)} {...props} />
-);
+const Kussy = props => {
+  return (
+    <Sprite
+      texture={PIXI.Texture.fromImage(kussy)}
+      {...props}
+    />
+  );
+};
 
-render(
-  <Stage width={800} height={600} options={{ backgroundColor: 0x10bb99 }}>
-    <Kussy x={100} y={100} />
-  </Stage>,
-  document.getElementById('root')
-);
-// render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  render() {
+    return (
+      <Stage width={800} height={600} options={{ backgroundColor: 0x10bb99 }}>
+        <Kussy x={100} y={100} />
+      </Stage>
+    );
+  }
+}
+ReactDOM.render(<App />, document.getElementById('root'));
