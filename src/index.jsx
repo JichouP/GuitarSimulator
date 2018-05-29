6c9216abbb03b042c3fcad6f5684a838d93c1312
JichouP <akkunn100@gmail.com>
2018-05-29 12:43:20 +0900

    冗長になっていたのを直した

--- src/index.jsx
+++ src/index.jsx
@@ -1,34 +1,25 @@
 //React
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Sprite, Stage } from 'react-pixi-fiber';
-import * as PIXI from 'pixi.js';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Link,
-  withRouter,
-} from 'react-router-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Sprite, Stage } from 'react-pixi-fiber';
+// import * as PIXI from 'pixi.js';
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Switch,
+//   Link,
+//   withRouter,
+// } from 'react-router-dom';
 //sound
-import { Howl, Howler } from 'howler';
+// import { Howl, Howler } from 'howler';
 import posToKey from './posToKey';
-import play from './play';
+// import play from './play';
 import beat from './beat';
-
+import Chord from './chord';
+const chord = new Chord();
 // play(4000, posToKey(1, 0), posToKey(2, 0), posToKey(3, 0), posToKey(4, 0), posToKey(5, 0), posToKey(6, 0));
 
-beat(
-  120,
-  8,
-  '10001011011010111000101101101011',
-  posToKey(1, 0),
-  posToKey(2, 1),
-  posToKey(3, 2),
-  posToKey(4, 3),
-  // posToKey(5, 3),
-  // posToKey(6, 0),
-);
+beat(120, 8, '10001011011010111000101101101011', ...chord.A);
 
 // audioSprite.currentTime = spriteData.a.start;
 // audioSprite.play();
--- src/posToKey.js
+++ src/posToKey.js
@@ -1,4 +1,4 @@
-export default (string,  fret) => {
+export default (...fret) => {
   const keys = [
     ['E4', 'F4', 'F#4', 'G4', 'G#4', 'A5', 'A#5', 'B5', 'C5', 'C#5', 'D5', 'D#5', 'E5', 'F5', 'F#5', 'G5', 'G#5', 'A6', 'A#6', 'B6', 'C6', 'C#6', 'D6'], 
     ['B4', 'C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A5', 'A#5', 'B5', 'C5', 'C#5', 'D5', 'D#5', 'E5', 'F5', 'F#5', 'G5', 'G#5', 'A6'], 
@@ -7,5 +7,9 @@ export default (string,  fret) => {
     ['A3', 'A#3', 'B3', 'C3', 'C#3', 'D3', 'D#3', 'E3', 'F3', 'F#3', 'G3', 'G#3', 'A4', 'A#4', 'B4', 'C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4'], 
     ['E2', 'F2', 'F#2', 'G2', 'G#2', 'A3', 'A#3', 'B3', 'C3', 'C#3', 'D3', 'D#3', 'E3', 'F3', 'F#3', 'G3', 'G#3', 'A4', 'A#4', 'B4', 'C4', 'C#4', 'D4'], 
   ];
-  return (keys[string - 1][fret]);
+  const result = [];
+  for (let i = 0; i < fret.length; i++){
+    if (fret[i] > -1 && fret[i] < 23) result.push(keys[i][fret[i]]);
+  }
+  return (result);
 };
