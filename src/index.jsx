605fb1af7bd1a0594d4fad198d02eb6715d95f40
JichouP <akkunn100@gmail.com>
2018-05-31 22:02:51 +0900

    コード完成した

--- src/index.jsx
+++ src/index.jsx
@@ -19,7 +19,7 @@ import Chord from './chord';
 const chord = new Chord();
 // play(4000, posToKey(1, 0), posToKey(2, 0), posToKey(3, 0), posToKey(4, 0), posToKey(5, 0), posToKey(6, 0));
 
-beat(120, 8, '10001011011010111000101101101011', ...chord.A);
+beat(120, 8, '10001011011010111000101101101011', ...chord.AM7);
 
 // audioSprite.currentTime = spriteData.a.start;
 // audioSprite.play();
